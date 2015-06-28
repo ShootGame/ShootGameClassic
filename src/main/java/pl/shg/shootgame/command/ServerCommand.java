9a446d900477193992d09144a6c88707b4f38696
TheMolkaPL <themolkapl@gmail.com>
2015-06-29 00:39:08 +0200

    Poprawa kodowania

--- src/main/java/pl/shg/shootgame/command/ServerCommand.java
+++ src/main/java/pl/shg/shootgame/command/ServerCommand.java
@@ -2,7 +2,7 @@
  * Copyright (C) 2014 TheMolkaPL - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
- * Written by Aleksander JagieĹ‚Ĺ‚o <themolkapl@gmail.com>, 2014
+ * Written by Aleksander Jagiełło <themolkapl@gmail.com>, 2014
  */
 package pl.shg.shootgame.command;
 
